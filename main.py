212926fabcb5a74be0a2c5afdb3c16b3f17a767d
Mario <oliveiramario006@gmail.com>
2025-02-17 15:07:37 -0300

    adicionando função de voz

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pyautogui
 import time
 import os
 import pyperclip
+import speech_recognition as sr
 
-def gameuser():
-    game = input("Which game do you want to play?")
+def gameuser(texto):
+    print("Which game do you want to play?")
     pyautogui.press("win")
     time.sleep(1)
-    pyautogui.write(game) #Write the name of the game the user is going to play.
+    pyautogui.write(texto) #Write the name of the game the user is going to play.
     time.sleep(1)
-    pyautogui.press("enter") 
+    pyautogui.press("enter")
 
 def party():
     friends = input('Which friend do you want to invite to the party?')
@@ -31,7 +32,21 @@ def party():
     pyautogui.write("Let's play") #Send the message inviting the friend to the party.
     time.sleep(1)
     pyautogui.press("enter") #Send the message.
+
+def voz():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Diha algo...")
+        audio = recognizer.listen(source)
+    try:
+        texto = recognizer.recognize_google(audio, language="pt-br")
+        gameuser(texto)
+    except sr.UnknownValueError:
+        print("Não entendi o que disse.")
+    except sr.RequestError as e:
+        print(f"Erro ao acessar o serviço: {e}")
+
 while True: 
     os.system("cls") #Loop running so that the previous commands are erased.
-    gameuser()
-    party()
+    voz()
+   
